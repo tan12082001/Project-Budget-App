3b907de82b6b243586a475984dc7d24ea6ff7790
Tanmayi Manku <tanmayitanum@gmail.com>
2023-11-21 19:49:35 +0530

    Add splash screen view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :splash_screen, only: [:index]
   resources :users
   resources :categories
   resources :expenditures
