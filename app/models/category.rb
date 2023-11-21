26ee6c1b8a67a604bf7c50dd21dbdd654f22891a
Tanmayi Manku <tanmayitanum@gmail.com>
2023-11-21 20:18:02 +0530

    add sum methods and nested routes

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ApplicationRecord
     belongs_to :user
     has_and_belongs_to_many :expenditures
+
+    def expenditures_sum
+        expenditures.sum(:amount)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
     has_many :categories
     has_many :expenditures, foreign_key: 'author_id'
+
+    def total_budget
+        expenditures.sum(:amount)
+    end
 end
