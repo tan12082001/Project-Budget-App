192c6fa0e5f57e5c9fb8c5e759c8082d836e8abc
Tanmayi Manku <tanmayitanum@gmail.com>
2023-11-23 00:01:07 +0530

    user before_action to allow except splash screen

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :null_session
-    before_action :authenticate_user!
+    before_action :authenticate_user!, unless: :splash_screen_controller?
     before_action :update_allowed_parameters, if: :devise_controller?
 
     protected
@@ -9,4 +9,12 @@ class ApplicationController < ActionController::Base
         devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :surname, :email, :password, :password_confirmation) }
         devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :surname, :email, :password, :current_password) }
     end
+
+    def splash_screen_controller?
+        controller_name == 'splash_screen'
+    end
+
+    # def after_sign_out_path_for(resource_or_scope)
+    #     unauthenticated_root_path
+    # end
 end
