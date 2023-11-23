13beec1d5a5d2f7e097417f8175fa51abee51098
Tanmayi Manku <tanmayitanum@gmail.com>
2023-11-23 10:54:52 +0530

    Add feature validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,9 +1,10 @@
 class Category < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :expenditures
-  # has_many :categories_expenditures, dependent: :destroy
-  # has_many :expenditures, through: :categories_expenditures
 
+  validates :name, presence: true, length: { maximum: 100 }, uniqueness: {scope: :user, message: 'Category exists!'}
+  validates :icon, presence: true
+  
   def expenditures_sum
     expenditures.sum(:amount)
   end
--- app/models/expenditure.rb
+++ app/models/expenditure.rb
@@ -1,7 +1,7 @@
 class Expenditure < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_and_belongs_to_many :categories
-  # has_many :categories_expenditures, dependent: :destroy
-  # has_many :categories, through: :categories_expenditures
-  # accepts_nested_attributes_for :categories_expenditures, allow_destroy: true
+
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :amount, presence: true, numericality: {money: true, greater_than_or_equal_to: 0}
 end
