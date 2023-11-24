0a8353bd1db6c8e4bd1cab0c830d6bd9672d0531
Tanmayi Manku <tanmayitanum@gmail.com>
2023-11-24 11:20:52 +0530

    Add class-names, ids required for styles.

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,9 +2,9 @@ class Category < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :expenditures
 
-  validates :name, presence: true, length: { maximum: 100 }, uniqueness: {scope: :user, message: 'Category exists!'}
+  validates :name, presence: true, length: { maximum: 100 }, uniqueness: { scope: :user, message: 'Category exists!' }
   validates :icon, presence: true
-  
+
   def expenditures_sum
     expenditures.sum(:amount)
   end
--- app/models/expenditure.rb
+++ app/models/expenditure.rb
@@ -3,5 +3,5 @@ class Expenditure < ApplicationRecord
   has_and_belongs_to_many :categories
 
   validates :name, presence: true, length: { maximum: 100 }
-  validates :amount, presence: true, numericality: {money: true, greater_than_or_equal_to: 0}
+  validates :amount, presence: true, numericality: { money: true, greater_than_or_equal_to: 0 }
 end
